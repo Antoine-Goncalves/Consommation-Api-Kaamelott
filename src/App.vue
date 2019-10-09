612a8c6540e1939372245015a04300fd8d9075f8
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-10-09 22:10:03 +0200

    add api to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+  <div id="app" class="container">
+    <h1 class="title is-1">Kaamelott</h1>
+    <div v-for="quote in quotes" :key="quote" class="text">
+      <p class="perceval">Perceval</p>
+
+      <div class="quote">{{quote.citation}}</div>
     </div>
-    <router-view />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 
-export default {};
+export default {
+  name: "app",
+  data() {
+    return {
+      quotes: null
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        `${"https://cors-anywhere.herokuapp.com/"}https://kaamelott.chaudie.re/api/all/livre/1/personnage/Perceval`
+      )
+      .then(response => (this.quotes = response.data.citation));
+  }
+};
 </script>
 
 <style lang="scss">
